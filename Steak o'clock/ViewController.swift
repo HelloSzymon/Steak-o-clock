7936ef04d73ea06d830d98acaa28cf2d9ee6fe1f
Szymon Wnuk <szwnuk1@gmail.com>
2022-04-10 22:00:45 +0200

    Initial Commit

---
+++ Steak o'clock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Steak o'clock
+//
+//  Created by Szymon Wnuk on 10/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
