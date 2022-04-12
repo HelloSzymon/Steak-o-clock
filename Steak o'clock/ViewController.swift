3f69f93eaf282c463af9aa0fe394854787289771
Szymon Wnuk <szwnuk1@gmail.com>
2022-04-12 19:04:23 +0200

    App done

--- Steak o'clock/ViewController.swift
+++ Steak o'clock/ViewController.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appTitle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        appTitle.text = ""
+        var charIndex = 0.0
+        let tittleText = "It's steak o'clock!"
+        for letter in tittleText {
+            print("-")
+            print(0.1 * charIndex)
+            print(letter)
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false)
+            { (timer) in self.appTitle.text?.append(letter)
+            }
+            charIndex += 1
+        }
+      
     }
 
 
